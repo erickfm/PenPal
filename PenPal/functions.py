cb50e61d9de317ef6068da975af3fa61c9d62600
Erick <efm.physics@gmail.com>
2023-06-10 18:44:21 -0700

    focusing the AI, increasing personability and originality

--- PenPal/functions.py
+++ PenPal/functions.py
@@ -23,7 +23,7 @@ def ask(prompt, write=False):
 
 
 def get_letter_prompt(company, title, skills, description, resume):
-    prompt = f"""Given a company, job title, desired skills, job description, and resume, generate an personable and effective cover letter from the applicant's resume to enhance their chances of securing an interview. 
+    prompt = f"""Given a company, job title, desired skills, job description, and resume, generate a personable, effective, and unique-sounding cover letter based on details from the applicant's resume. Avoid mentioning skills the resume does not mention or imply. Avoid copying the resume text verbatim. 
     Company: {company}
     Job title: {title}
     Desired skills: {skills}
